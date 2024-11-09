04c8f3b5a2104cbafc9f629f074b80ea334d8ea0
Adnankhan10s <97806852+Adnankhan10s@users.noreply.github.com>
2024-11-09 01:44:33 -0800

    add process

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,15 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import process from 'process'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  resolve: {
+    alias: {
+      process: 'process/browser',  // Polyfill process
+    },
+  },
+  define: {
+    'process.env': process.env, // Inject environment variables
+  },
 })
